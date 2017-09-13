1d6d777d8b1ada2ca6c695e4d8ef7b7d4698759c
Sandarsh Chavalmane <schavalmane@fb.com>
2017-09-12 18:08:45 -0700

    no changes

---
+++ test2c.py
@@ -0,0 +1,17 @@
+#Function to count the Vowels
+#python looks for this test1b.py script in same directory
+form test1b import isVowel
+#above or below 
+VOWELS= {AEIOUaeiou}
+def numVowl(a)
+   return a in VOWELS 
+    return 0
+
+z=[aeiouootft]
+test=count(z)
+
+if __name__ =='__main__':
+   
+
+
+print (
